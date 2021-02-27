4447d7105b982d65451c58ffd372fbd2275e71db
Alban Lorillard <alban.lorillard@gmail.com>
2021-02-27 20:20:14 +0100

    up

--- src/App.js
+++ src/App.js
@@ -2,24 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Alban Lorillard</h1>
+                <h2>Ingénieur logiciel web & mobile pour E-Voyageur SNCF</h2>
+                <p>
+                    <ul>
+                        <li>Diplômé en ingénieurie informatique à Polytech Nantes</li>
+                        <li>Diplômé en innovation et transformation numérique à l'Université de Nantes</li>
+                    </ul>
+                </p>
+            </header>
+            <main>
+                <h2></h2>
+            </main>
+        </div>
+    );
 }
 
 export default App;
