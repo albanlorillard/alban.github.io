543472fa48a79b429ac2a4bce669523d935a8542
Alban Lorillard <alban.lorillard@gmail.com>
2021-02-27 23:15:11 +0100

    first version

--- src/App.js
+++ src/App.js
@@ -4,18 +4,27 @@ import './App.css';
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <h1>Alban Lorillard</h1>
-                <h2>Ingénieur logiciel web & mobile pour E-Voyageur SNCF</h2>
-                <p>
+            <header>
+                <h1>&lt;Alban/&gt;</h1>
+                <nav className="navigation">
                     <ul>
-                        <li>Diplômé en ingénieurie informatique à Polytech Nantes</li>
-                        <li>Diplômé en innovation et transformation numérique à l'Université de Nantes</li>
+                        <li><a href="https://www.linkedin.com/in/albanlorillard/" target="_blank">LinkedIn</a></li>
+                        <li><a href="https://github.com/albanlorillard" target="_blank">Github</a></li>
+                        <li><a href="https://twitter.com/alban_lorillard" target="_blank">Twitter</a></li>
                     </ul>
-                </p>
+                </nav>
             </header>
             <main>
-                <h2></h2>
+                <div className="center">
+                    <h1>Alban Lorillard</h1>
+                    <h2>Ingénieur logiciel web & mobile pour <a href="https://ouitalk.oui.sncf/le-groupe/qui-sommes-nous" target="_blank">E-Voyageur SNCF</a></h2>
+                    <p>
+                        <ul>
+                            <li>Diplômé en ingénieurie informatique à <a href="https://polytech.univ-nantes.fr/" target="_blank">Polytech Nantes - Bac+5</a></li>
+                            <li>Diplômé en innovation et transformation numérique à l'<a href="https://www.disruptcampusnantes.com/" target="_blank">Université de Nantes</a> - Bac+5</li>
+                        </ul>
+                    </p>
+                </div>
             </main>
         </div>
     );
