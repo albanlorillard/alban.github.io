6254c700ab1205b777496261de4d6bec60e61391
Alban Lorillard <alban.lorillard@gmail.com>
2021-12-28 22:25:46 +0100

    feat: init gatsby website

--- src/pages/AboutPages/AboutPages.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import AboutPages from './App';
-
-test('renders learn react link', () => {
-  render(<AboutPages />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
